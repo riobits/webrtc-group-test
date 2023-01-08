9ca048335ae4c5c726a7f67b133c07db9dc367a9
riobits <23579928+riobits@users.noreply.github.com>
2023-01-09 01:57:21 +0300

    fix deploy bug

--- src/server.ts
+++ src/server.ts
@@ -26,4 +26,6 @@ io.on('connection', (socket) => {
   })
 })
 
-server.listen(3000, () => console.log('Listening on port 3000'))
+const port = process.env.PORT || 3000
+
+server.listen(port, () => console.log(`Listening on port ${port}`))
